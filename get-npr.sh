b4a54ddda00f3345e437c48b798e4df900770f9b
JM Addington <jm@jmaddington.com>
2020-07-26 09:33:36 -0400

    Updated for new show formats

---
+++ get-npr.sh
@@ -0,0 +1,50 @@
+#Download info
+
+#The URL of the progam to download should be passed as the only argument. The generic show
+#or a specific date can be entered
+#
+#i.e., ./get-npr.sh https://www.npr.org/programs/all-things-considered/
+#or ./get-npr.sh https://www.npr.org/programs/all-things-considered/2020/07/24/894981617?showDate=2020-07-24
+url=$1
+
+#The direcotry that downloads should be put into
+dir=./downloads/
+
+#The filename we'll download the show to.
+file=npr.html
+
+cd $dir
+
+wget -O $file $1
+
+#Find the mp3 links in the file
+cat $file|grep audio-tool-download|awk '{print $4}'|sed 's/\"/\ /g'|sed 's/\href=/\ /g'|while read -r mp3
+do
+
+  #Download the mp3
+  echo "Downloading" $mp3
+  wget $mp3
+
+   title=`echo $mp3|sed 's/\?/\ /g'|awk '{print $1}'`
+
+done
+
+#This should eventually be included in above, to avoid re-processing all MP3s
+find|grep \?|while read -r mp3
+do
+
+  #Put dates into variables
+  year=`date +"%Y"`
+  month=`date +"%m"`
+  day=`date +"%d"`
+
+  folder="$year-$month-$day"
+
+  #Give MP3s sensible filenames
+  newFn=`echo $mp3|sed 's/\?/\ /g'|awk '{print $1}'`
+  echo Moving $mp3 to $newFn
+
+  mkdir -p $folder
+  mv $mp3 $folder/$newFn
+
+done
